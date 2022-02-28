9571c13a146dd58fd9870c19f5d112d2127c5a80
LizMackle <liz.mackle@outlook.com>
2022-02-28 16:21:18 +0800

    working code amended

--- routes/api.js
+++ routes/api.js
@@ -10,130 +10,48 @@ const dbPath = path.join(__dirname, "..", "db", "db.json");
  */
 
 function retrieveNotes(){
-
     return JSON.parse(fs.readFileSync(dbPath, "utf-8") || "[]");
 }
-
 
 function saveNotes(notes){
     fs.writeFileSync(dbPath, JSON.stringify(notes), "utf-8");
 }
-
-router.get("/notes", (req, res) => {
-    
-    // get the content of db.json
-
-    const notes = retrieveNotes();;
-
+router.get("/api/notes", (req, res) => {
+    const notes = retrieveNotes();
     console.log(notes);
-
-
-    // send them back
-    res.json(notes);
-    
+    res.json(notes); // send them back
 });
 
 
-router.post('/notes', (req, res) => {
-
-    // get the req body
+router.post("/api/notes", (req, res) => {
     const {title, text} = req.body;
-
-
     console.log(title, text);
-
-    // create a new note in db json
+    // creates a new note in db json
     const newNote = {
         id: uuid.v4(),
         title,
         text,
-    }
-
+    };
     // get all the existing notes in db json
     const existingNotes = retrieveNotes();
-
-    // add this new note to the existing
+    // add new note to existing
     existingNotes.push(newNote);
-
-    // resave 
+    // save note
     saveNotes(existingNotes);
-
-    // send back response to client, with the new note in the res body
+    // send back
     res.json(newNote);
-
 });
 
-
-// update
-router.put('/notes/:id', (req, res) => {
-
-    // get all the notes
+router.delete("/api/notes:id", (req, res) => {
     const notes = retrieveNotes();
-
-    // find the note
-    const indexFound = notes.findIndex((note) => note.id === req.params.id);
-
-    if(indexFound === -1){
-        return res.status(400).json({
-            error: 'NOT FOUND !!!!!'
-        });
-    }
-    
-    // update it
-    const noteFound = notes[indexFound]
-    notes[indexFound].title = req.body.title || noteFound.title;
-    notes[indexFound].text = req.body.text || noteFound.text;
-
-    // resave
-    saveNotes(notes);
-
-    res.json(noteFound);
-
-});
-
-
-// getting a specific note
-router.get('/notes/:id', (req, res) => {
-
-    // get all the notes
-    const notes = retrieveNotes();
-
-    // find the note with matching id
-    const found = notes.find((note) => note.id === req.params.id);
-
-    if(!found){
-        res.status(400).json({
-            error: 'not found!!'
-        })
-    }else{
-        // send back the note
-        res.json(found);
-    }
-
-
-
-});
-
-
-router.delete('/notes/:id', (req, res) => {
-
-    // get all the existing notes
-    const notes = retrieveNotes();
-
-    // get rid of the target note with the given id
+    // filters to target note
     const filtered = notes.filter((note) => note.id !== req.params.id);
-
-
     // resave
     saveNotes(filtered);
-
-
-    // send back res
+    // send back
     res.json({
         data: "ok",
     })
-
-
 });
 
 module.exports = router;
